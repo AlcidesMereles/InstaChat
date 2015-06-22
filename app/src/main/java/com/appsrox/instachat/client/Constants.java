9d23ede912e37bb06ad091a094fe0fae4c068822
Alcides Mereles <dante1587@gmail.com>
2015-06-22 00:27:30 -0300

    Initial commit

---
+++ app/src/main/java/com/appsrox/instachat/client/Constants.java
@@ -0,0 +1,18 @@
+package com.appsrox.instachat.client;
+
+/**
+ * @author appsrox.com
+ *
+ */
+public interface Constants {
+
+    /**
+     * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     */
+	String SERVER_URL = "http://1-dot-daparadarse-983.appspot.com";
+
+    /**
+     * Google API project id registered to use GCM.
+     */
+	String SENDER_ID = "38175080730";
+}
